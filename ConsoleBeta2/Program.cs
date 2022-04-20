fe9242e2f7cd3bef24d00f4bb8c7d5dd50e1ff5f
vidapogosoft <vidapogosoft@hotmail.com>
2022-04-19 22:22:37 -0500

    Talleres dia 1
    programas de consola
    proyecto de api rest
    19 de abril

---
+++ ConsoleBeta2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleBeta2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es su nombre?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Bienvenido, {name}, el dia {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
