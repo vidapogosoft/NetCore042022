589accbdfe9a68002ef831833a62c1aeaf6e6b3e
vidapogosoft <vidapogosoft@hotmail.com>
2022-04-26 18:48:44 -0500

    Create QRY_TAB_DEMO_AEROCLUB.sql

---
+++ QRY_TAB_DEMO_AEROCLUB.sql
@@ -0,0 +1,77 @@
+use master
+go
+
+Create database  DBAeroClub
+go
+
+use DBAeroClub
+go
+
+Create table  Personas
+(
+    IdPersona  int primary key identity(1,1),
+    Identificacion varchar(20),
+    Nombres varchar(200),
+    Apellidos varchar(200),
+    NombresCompletos varchar(400),
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
+go
+
+Create table  Pilotos
+(
+    IdPiloto  int primary key identity(1,1),
+    IdPersona int,
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
+
+Create table  Instructores
+(
+    IdInstructor  int primary key identity(1,1),
+    IdPersona int,
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
+
+Create table  Aeronave
+(
+    IdAeronave  int primary key identity(1,1),
+    IdPersona int,
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
+
+
+Create table  Horario
+(
+    IdHorario  int primary key identity(1,1),
+    Horario varchar(max),
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
+
+
+Create table  Vuelos
+(
+    IdVuelo  int primary key identity(1,1),
+    IdInstructor  int,
+    IdHorario  int,
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
+
+Create table  PilotoVuelos
+(
+    IdPilotoVuelos  int primary key identity(1,1),
+    IdVuelo  int,
+    IdPiloto  int,
+    AnioVuelo  int,
+    DiaInicio  int,
+    DiaFin  int,
+    HoraInicio  int,
+    HoraFin  int,
+    FechaIngreso datetime,
+    Estado varchar(50)
+)
