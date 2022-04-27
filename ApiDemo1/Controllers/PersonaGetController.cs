ff72eb6da647f1bfb639f1ad3985244b2f8c0d55
vidapogosoft <vidapogosoft@hotmail.com>
2022-04-26 23:03:47 -0500

    controller new

---
+++ ApiDemo1/Controllers/PersonaGetController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ApiDemo1.Interfaces;
+
+
+namespace ApiDemo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaGetController : ControllerBase
+    {
+        private readonly IPersonasGet _Persona;
+
+        public PersonaGetController (IPersonasGet iper)
+        {
+            _Persona = iper;
+        }
+
+        [HttpGet("PerosnasAll1")]
+        public IActionResult Get1()
+        {
+            return Ok(_Persona.ListPersonaAll);
+        }
+
+        [HttpGet("PerosnasAll2")]
+        public IActionResult Get2()
+        {
+            return Ok(_Persona.ListPersonaAll2);
+        }
+    }
+}
