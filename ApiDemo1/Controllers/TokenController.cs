c4f8b765577f962ebf41079cdb4224f17e74140e
vidapogosoft <vidapogosoft@hotmail.com>
2022-05-04 18:50:07 -0500

    Taller dia 6
    Api Token

---
+++ ApiDemo1/Controllers/TokenController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using ApiDemo1.Modelo.DTO;
+
+namespace ApiDemo1.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthInfo user)
+        {
+            var token = user.Username + user.Password;
+
+            if(token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ ApiDemo1/Modelo/DTO/AuthInfo.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ApiDemo1.Modelo.DTO
+{
+    public class AuthInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
