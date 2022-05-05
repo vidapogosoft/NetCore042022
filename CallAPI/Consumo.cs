031f00e09e1d0015be7482030982661b09df28c8
vidapogosoft <vidapogosoft@hotmail.com>
2022-05-04 21:06:50 -0500

    consumo de apis con JWT

--- CallAPI/Consumo.cs
+++ CallAPI/Consumo.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
 using CallAPI.Model;
+using System.Net.Http.Headers;
 
 namespace CallAPI
 {
     public class Consumo
     {
         public List<DTOPersona> ListPersona;
+        public string Token = string.Empty;
 
         HttpClient _Client;
 
@@ -27,12 +29,19 @@ namespace CallAPI
             {
                 try
                 {
+                    //Metodos con seguridad del JWT
+                    PostTokenApi();
 
-                    GetApi1();
-                    PostApi1();
+                    if (!string.IsNullOrEmpty(Token))
+                    {
+                        GetApiAuth();
+                    }
+                        //Metodos sin seguridad del JWT
+                        //GetApi1();
+                        //PostApi1();
 
 
-                    System.Threading.Thread.Sleep(5000);
+                        System.Threading.Thread.Sleep(5000);
 
                 }
                 catch (Exception ex)
@@ -40,6 +49,63 @@ namespace CallAPI
 
                     Console.WriteLine(ex.Message);
                 }
+            }
+        }
+
+        public async void GetApiAuth()
+        {
+            var uriget = new Uri("http://localhost:64508/api/PersonaGet/PerosnasAll1");
+
+            if (string.IsNullOrEmpty(Token))
+            {
+                Console.WriteLine("No existe token");
+            }
+            else
+            {
+                //Especifico TOKEN JWT
+                _Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+                
+                var responseget = await _Client.GetAsync(uriget);
+
+                if (responseget.IsSuccessStatusCode)
+                {
+                    var contentget = await responseget.Content.ReadAsStringAsync();
+
+                    ListPersona = JsonConvert.DeserializeObject<List<DTOPersona>>(contentget);
+
+                }
+
+                if (ListPersona.Count > 0)
+                {
+                    Console.WriteLine("get Exitoso: " + ListPersona.Count.ToString());
+                }
+
+            }
+
+
+        }
+
+        public async void PostTokenApi()
+        {
+            var uri = new Uri("http://localhost:64508/api/token/authenticate");
+
+            TokenAuth auth = new TokenAuth();
+            auth.Username = "demo";
+            auth.Password = "123456";
+
+            var json = JsonConvert.SerializeObject(auth);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage ResponseToken = null;
+
+            ResponseToken = await _Client.PostAsync(uri, content);
+
+            if (ResponseToken.IsSuccessStatusCode)
+            {
+                Token = ResponseToken.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                Console.WriteLine(Token);
             }
         }
 
---
+++ CallAPI/Model/TokenAuth.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace CallAPI.Model
+{
+    public class TokenAuth
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
